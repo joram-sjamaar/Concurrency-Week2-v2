ce860a1e9b5beb1d10049df2adacf0c38d4cdcff
Joram Sjamaar <47634296+joram-sjamaar@users.noreply.github.com>
2019-11-03 00:57:17 -0700

    FWAF

--- src/Apl.java
+++ src/Apl.java
@@ -7,19 +7,21 @@ public class Apl {
     private static final int NR_OF_CUSTOMERS = 9;
 
     public static void main(String[] args) {
+
         // TODO: Je kanker moeder
         PrinterManager printerManager = new PrinterManager();
-        Thread [] customer;
+        Thread[] customer;
         Thread printer;
         customer = new Thread[NR_OF_CUSTOMERS];
 
         printer = new Printer(printerManager);
         printer.start();
 
-        for(int i = 0; i<NR_OF_CUSTOMERS; i++){
-            customer[i] = new LittleFish("c"+i , printerManager);
+        for (int i = 0; i < NR_OF_CUSTOMERS; i++) {
+            customer[i] = new LittleFish("c" + i, printerManager);
             customer[i].start();
         }
+
     }
 
 }
